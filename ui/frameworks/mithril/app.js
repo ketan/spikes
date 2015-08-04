561057f005e7c418ce05233ada2c09afc0d6fed8
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2015-08-04 14:41:09 +0530

    Controllers now fetch the model based on parameters

--- ui/frameworks/mithril/app.js
+++ ui/frameworks/mithril/app.js
@@ -30,9 +30,13 @@ var data = [
 ]
 
 var go = {config: {}};
-var edit = go.config.edit = {};
+var edit = go.config.edit = {
+    stage: {},
+    job: {}
+};
 
-edit.Pipeline = Array;
+edit.Pipelines = Array;
+edit.Pipelines.prototype.
 
 edit.Stage = function(name, jobs) {
     this.name = m.prop(name);
@@ -45,9 +49,9 @@ edit.Job = function(name){
 
 edit.vm = {
     init: function(data){
-        var pipeline = this.pipeline = new edit.Pipeline();
+        var pipelines = this.pipelines = new edit.Pipelines();
         data.map(function(stage){
-            pipeline.push(new edit.Stage(stage.name, stage.jobs.map(function(job){
+            pipelines.push(new edit.Stage(stage.name, stage.jobs.map(function(job){
                 return new edit.Job(job.name);
             })));
         });
@@ -59,11 +63,41 @@ edit.controller = function(){
 };
 
 edit.view = function(){
-    return m("ul", edit.vm.pipeline.map(function(stage){
-        return m("li", stage.name(), m("ul", stage.jobs().map(function(job){
-            return m("li", job.name());
-        })))
+    return m("ul", edit.vm.pipelines.map(function(stage){
+        return m("li",
+                 m("a", {"href": "#/edit/stage/" + stage.name()}, stage.name()),
+                 m("ul", stage.jobs().map(function(job){
+                     return m("li",
+                              m("a", {"href": "#/edit/job/" + stage.name() + "/" + job.name() }, job.name()));
+                 })))
     }));
 }
 
+edit.stage.controller = function(){
+    var name = m.route.param("id");
+    return {
+        "stage" : edit.vm.pipelines.find(function(stage){
+            return stage.name() == name;
+        })
+    };
+}
+
+edit.stage.view = function(controller) {
+    return m("div", controller.stage.name());
+}
+
+edit.job.controller = function(){
+    return { "id" : m.route.param("id") };
+}
+
+edit.job.view = function(controller) {
+    return m("div", controller.id);
+}
+
 m.render(document.getElementById('pipeline'), edit);
+
+m.route.mode = "hash";
+m.route(document.getElementById('editArea'),"/edit/stage/Build", {
+    "/edit/stage/:id" : edit.stage,
+    "/edit/job/:id..." : edit.job
+});
